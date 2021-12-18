0113083999b75b0ec14512f4336f7672d29ba401
tsuchiya <tsuchiya@bengo4.com>
2021-12-18 22:42:30 +0900

    Add comment

--- bin/importExample.php
+++ bin/importExample.php
@@ -17,8 +17,8 @@ Yii::import('application.models.*');
 
 echo get_include_path() . PHP_EOL;
 
-echo class_exists(SiteController::class) . PHP_EOL;
-echo class_exists(UserIdentity::class) . PHP_EOL;
-echo class_exists(ContactForm::class) . PHP_EOL;
-echo class_exists(YiiNameSpacedController::class) . PHP_EOL;
-echo class_exists(PSR4NameSpacedController::class) . PHP_EOL;
+echo class_exists(SiteController::class) . PHP_EOL; // include from include_path
+echo class_exists(UserIdentity::class) . PHP_EOL; // include from include_path
+echo class_exists(ContactForm::class) . PHP_EOL; // include from include_path
+echo class_exists(YiiNameSpacedController::class) . PHP_EOL; // include from alias path
+echo class_exists(PSR4NameSpacedController::class) . PHP_EOL; // include from composer autoloader
