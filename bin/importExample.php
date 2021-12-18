7921ced45fddfaceacd1139b2fbec54f703f5097
tsuchiya <tsuchiya@bengo4.com>
2021-12-18 22:46:17 +0900

    Add ClassMapController

--- bin/importExample.php
+++ bin/importExample.php
@@ -14,6 +14,7 @@ Yii::setPathOfAlias('application', __DIR__ . '/../public/protected');
 Yii::import('application.controllers.*');
 Yii::import('application.components.*');
 Yii::import('application.models.*');
+Yii::$classMap['ClassMapController'] = __DIR__ . '/../public/classmaps/ClassMapController.php';
 
 echo get_include_path() . PHP_EOL;
 
@@ -22,3 +23,4 @@ echo class_exists(UserIdentity::class) . PHP_EOL; // include from include_path
 echo class_exists(ContactForm::class) . PHP_EOL; // include from include_path
 echo class_exists(YiiNameSpacedController::class) . PHP_EOL; // include from alias path
 echo class_exists(PSR4NameSpacedController::class) . PHP_EOL; // include from composer autoloader
+echo class_exists(ClassMapController::class) . PHP_EOL; // include from classmap
---
+++ public/classmaps/ClassMapController.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+class ClassMapController extends Controller
+{
+
+}
